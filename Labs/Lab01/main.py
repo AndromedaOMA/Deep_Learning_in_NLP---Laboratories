f8d3eddf2ad3bc6bd69f16d22a1b5e17bff548b5
Olaru Marius-Alexandru <marius.alexandru.olaru@gmail.com>
2025-10-02 13:23:15 +0300

    first lab

---
+++ Labs/Lab01/main.py
@@ -0,0 +1,24 @@
+from nltk.corpus import wordnet
+
+
+def syn_and_ant(word: str):
+    synonyms = []
+    antonyms = []
+    definitions = []
+
+    for syn in wordnet.synsets(word):
+        definitions.append(syn.definition())
+        for l in syn.lemmas():
+            synonyms.append(l.name())
+
+            if l.antonyms():
+                antonyms.append(l.antonyms()[0].name())
+
+    print('Syn', set(synonyms))
+    print('Ant', set(antonyms))
+    print('DEF', set(definitions))
+
+
+if __name__ == '__main__':
+    word = input('Enter your word!\n')
+    syn_and_ant(word)
