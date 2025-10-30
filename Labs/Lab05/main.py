3b5154729499984fa52f91258fc9c3dd61b73f2d
unknown <tudor.ezaru@yahoo.com>
2025-10-30 13:16:59 +0200

    add: lab05 wiki articles

---
+++ Labs/Lab05/main.py
@@ -0,0 +1,38 @@
+import wikipediaapi
+
+wiki_wiki = wikipediaapi.Wikipedia(user_agent='MyProjectName (merlin@example.com)', language='en')
+
+
+def read_content(page: str) -> str:
+    p_wiki = wiki_wiki.page(page)
+    return p_wiki.text
+
+
+def get_category_articles(categorymembers, size=15, level=0, max_level=1):
+    articles = []
+
+    for c in categorymembers.values():
+        if c.ns == wikipediaapi.Namespace.MAIN:
+            articles.append(c.title)
+            if len(articles) >= size:
+                return articles
+
+        elif c.ns == wikipediaapi.Namespace.CATEGORY and level < max_level:
+            sub_articles = get_category_articles(c.categorymembers, size - len(articles), level + 1, max_level)
+            articles.extend(sub_articles)
+            if len(articles) >= size:
+                return articles
+
+    return articles
+
+
+categories = ['Science', 'Music', 'Cooking']
+titles = []
+for c in categories:
+    current_category = wiki_wiki.page(f"Category:{c}")
+    cat = get_category_articles(current_category.categorymembers, 6)
+    titles.extend(cat[1:])
+
+for title in titles:
+    print("==================================")
+    print(read_content(title))
